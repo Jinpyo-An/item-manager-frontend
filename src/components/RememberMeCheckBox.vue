5f0438c732ca23cd1a73fecb1dcf3475edb1bbca
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-08-22 13:25:50 +0900

    feat: 로그인 페이지 구현
    로그인 페이지 구현
    
    resolves: #1

---
+++ src/components/RememberMeCheckBox.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="remember-me">
+		<label class="checkbox-label">
+			<input type="checkbox" id="rememberMe" />
+			<span>Remember me</span>
+		</label>
+		<a href="#" class="forgot-password">Forgot password?</a>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.remember-me {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-top: 20px;
+}
+
+.checkbox-label {
+	display: flex;
+	align-items: center;
+	gap: 5px; /* 체크박스와 텍스트 사이의 간격 */
+}
+
+.forgot-password {
+	color: #4a90e2;
+	font-size: 14px;
+	text-decoration: none;
+}
+</style>
---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<button class="submit-button">Continue</button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.submit-button {
+	width: 100%;
+	padding: 15px;
+	font-size: 16px;
+	cursor: pointer;
+	margin-top: 20px;
+	color: #ffffffff; /* white */
+	background: #5269cdff; /* primary-500 */
+	opacity: 1;
+	border: none;
+	border-radius: 12px;
+}
+</style>
