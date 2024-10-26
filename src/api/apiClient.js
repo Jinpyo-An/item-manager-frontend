27244336b038806a78236e09444cd2efc6e154f9
Jinpyo-An <dkswlsvy3312@gmail.com>
2024-10-26 19:36:55 +0900

    deploy 변경사항 적용

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL: `http://capstone-api.wisoft.io/item-manager`, // 배포용 url로 변경
+	baseURL: `https://capstone-api.wisoft.io/item-manager`, // 배포용 url로 변경
 	headers: {
 		'Content-Type': 'application/json',
 	},
