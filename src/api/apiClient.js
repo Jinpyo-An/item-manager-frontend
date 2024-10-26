1372b243ff6ddde431d0d049051add8ce847f1d4
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-10-26 19:40:10 +0900

    변경사항 적용

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL: `http://${window.location.hostname}:3000`,
+	baseURL: `http://${window.location.hostname}:3001`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
